93a2e0a8284f8942f3b6877712fb4e759c960b15
myasindogac59 <yasindogac@gmail.com>
2025-03-22 04:21:11 +0300

    created Category model and updated requirements.txt

--- todo/admin.py
+++ todo/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import (Todo)
+from .models import (Todo, Category)
 # Register your models here.
 
 
@@ -11,3 +11,4 @@ class TodoAdmin(admin.ModelAdmin):
     'updated_at',
   ]
 admin.site.register(Todo, TodoAdmin)
+admin.site.register(Category)
